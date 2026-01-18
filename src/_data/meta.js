3ec30daf7ff2c0942942eea80c55c41a57cabca3
Lea Rosema <lea.rosema@adesso.de>
2026-01-19 00:39:32 +0100

    chore: fix url config

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,5 +1,5 @@
 export default {
   siteName: "Lea's CPUX Preparation Site",
   author: 'Lea Rosema',
-  root: process.env.ENV === 'production' ? 'https://lea11y.de' : ''
+  root: process.env.ENV === 'production' ? 'https://learosema.github.io/cpux-prep/' : ''
 }
