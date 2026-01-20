2c44489779e4343ab66e6925f5843db62d600ed5
Lea Rosema <lea.rosema@adesso.de>
2026-01-21 00:56:23 +0100

    chore: add prev next links

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,7 +1,35 @@
-import { parseHTML } from 'linkedom';
-import { createIndex } from 'pagefind'
+import { parseHTML } from "linkedom";
+import { createIndex } from "pagefind";
+import { readFile } from "node:fs/promises";
 
-export default function (eleventyConfig) {
+async function createSortedCollection(eleventyConfig) {
+  const src = await readFile("src/index.md", "utf8");
+  const linkRegex = /\[.*?\]\((.*?)\)/g;
+  const hrefs = ["./src/index.md"];
+  let m;
+  while ((m = linkRegex.exec(src)) !== null) {
+    if (m[1]?.startsWith("https://") === false) {
+      const file = "./src/" + m[1];
+      hrefs.push(file);
+    }
+  }
+  eleventyConfig.addCollection("toc", function (collectionsApi) {
+    return collectionsApi
+      .getAll()
+      .filter((a) => hrefs.indexOf(a.inputPath) >= 0)
+      .sort(function (a, b) {
+        const aIndex = hrefs.indexOf(a.inputPath);
+        const bIndex = hrefs.indexOf(b.inputPath);
+
+        return aIndex - bIndex;
+      });
+  });
+}
+
+
+export default async function (eleventyConfig) {
+  eleventyConfig.addPlugin(createSortedCollection);
+
   eleventyConfig.addPassthroughCopy('src/css')
   eleventyConfig.addPassthroughCopy('src/js')
   eleventyConfig.addPassthroughCopy('src/images')
@@ -17,22 +45,27 @@ export default function (eleventyConfig) {
   })
   
   eleventyConfig.addGlobalData('layout', () => 'default.njk')
+
+
+
+
+  eleventyConfig.addGlobalData("layout", () => "default.njk");
   // Set global permalinks to resource.html style
-	eleventyConfig.addGlobalData("permalink", () => {
-		return (data) =>
-			`${data.page.filePathStem}.${data.page.outputFileExtension}`;
-	});
+  eleventyConfig.addGlobalData("permalink", () => {
+    return (data) =>
+      `${data.page.filePathStem}.${data.page.outputFileExtension}`;
+  });
 
   eleventyConfig.addGlobalData("eleventyComputed.title", () => {
     return (data) => {
-      if (data.page.inputPath?.endsWith('.md')) {
+      if (data.page.inputPath?.endsWith(".md")) {
         const titleRegex = /^\#\s(.*?)\r?\n/;
         const match = data.page.rawInput?.match(titleRegex);
-        return match ? match[1] : '';
+        return match ? match[1] : "";
       }
-      return '';
-    }
-  })
+      return "";
+    };
+  });
 
   eleventyConfig.addTransform('htmlpost', function (content) {
     if (((this.page.outputPath || "").endsWith(".html")) === false) {
@@ -40,11 +73,11 @@ export default function (eleventyConfig) {
     }
     const { document } = parseHTML(content);
     const isRelativeLink = (href) => /^https?:\/\//.test(href) === false;
-    const anchors = document.querySelectorAll('a[href]')
+    const anchors = document.querySelectorAll("a[href]");
     for (const anchor of anchors) {
-      const href = anchor.getAttribute('href') || ''
-      if (isRelativeLink(href) && href.endsWith('.md')) {
-        anchor.setAttribute('href', href.replace(/\.md$/, '.html'))
+      const href = anchor.getAttribute("href") || "";
+      if (isRelativeLink(href) && href.endsWith(".md")) {
+        anchor.setAttribute("href", href.replace(/\.md$/, ".html"));
       }
     }
 
@@ -56,26 +89,26 @@ export default function (eleventyConfig) {
       }
     }
     return document.toString();
-  })
+  });
 
 
 
   eleventyConfig.on('eleventy.after', async () => {
     console.info('creating search index files')
     const { index } = await createIndex();
-    index.addDirectory({ path: 'dist' });
-    await index.writeFiles({outputPath: 'dist/pagefind'});
-    console.info('done.')
-  })
+    index.addDirectory({ path: "dist" });
+    await index.writeFiles({ outputPath: "dist/pagefind" });
+    console.info("done.");
+  });
 
   return {
-    markdownTemplateEngine: 'njk',
-    htmlTemplateEngine: 'njk',
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
     dir: {
-      input: 'src',
+      input: "src",
       includes: "_includes",
       layouts: "_layouts",
-      output: 'dist',
-    }
-  }
+      output: "dist",
+    },
+  };
 }
